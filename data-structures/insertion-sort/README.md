04e752abf0936eff3d05495b87aa5d6be54c8e89
lilbonito <sauteboy@gmail.com>
2019-10-02 00:47:18 -0700

    done

---
+++ data-structures/insertion-sort/README.md
@@ -0,0 +1,14 @@
+# Insertion Sort
+
+
+## Challenge
+To read and translate pseudo-code for insertion sort function/algorithm.
+
+## Approach & Efficiency
+
+time: 0(2n) --> 0(n)
+space: 0(1)
+
+## Solution
+
+
---
+++ data-structures/insertion-sort/__tests__/insertion-sort.test.js
@@ -0,0 +1,9 @@
+const insertionSort = require('../insertion-sort');
+
+describe('Insertion Sort', () => {
+  it('should sort the items', () => {
+    const insert = [5, 2, 4, 6, 1, 3];
+    const sorted = insertionSort(insert);
+    expect(sorted).toEqual([1, 2, 3, 4, 5, 6])
+  });
+});
