66db804e687694deef07d010cbcf11e1b1e6e740
lilbonito <sauteboy@gmail.com>
2019-10-02 10:24:29 -0700

    fixed linter errors

--- data-structures/insertion-sort/__tests__/insertion-sort.test.js
+++ data-structures/insertion-sort/__tests__/insertion-sort.test.js
@@ -4,6 +4,6 @@ describe('Insertion Sort', () => {
   it('should sort the items', () => {
     const insert = [5, 2, 4, 6, 1, 3];
     const sorted = insertionSort(insert);
-    expect(sorted).toEqual([1, 2, 3, 4, 5, 6])
+    expect(sorted).toEqual([1, 2, 3, 4, 5, 6]);
   });
 });
